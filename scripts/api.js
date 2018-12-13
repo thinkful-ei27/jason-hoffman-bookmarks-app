69a93a679c5a684fad48b3cb6e6ab64f05362c7a
publikwerker <hello@publikwerks.com>
2018-12-13 14:28:26 -0800

    added new bookmark and retrieved new array from api

--- scripts/api.js
+++ scripts/api.js
@@ -1,18 +1,43 @@
 
 
-const getAPI = (function(callback) {
+const api = (function(callback) {
+    console.log('api ran');
     const BASE_URL = 'https://thinkful-list-api.herokuapp.com/jason/bookmarks';
 
     const getBookmarks = (function(callback) {
-        $.getJSON(BASE_URL, (data) => {
+        console.log('getBookmarks ran');
+        const bookmarkList = $.getJSON(BASE_URL, (data) => {
             console.log(data);
+        });
+        console.log('const bookmarkList= ' + bookmarkList);
+        return bookmarkList;
+        
+    });
+
+    const createBookmarks = (function(linkTitle, callback) {
+        console.log('createBookmarks ran');
+        const newBookmark = JSON.stringify({
+            "desc": null,
+            "id": "cjpn2jd2t001j0kwn2k9milk3",
+            "rating": null,
+            "title": `"${linkTitle}"`,
+            "url": "http://www.bgirl.com"  
+        });
+
+        $.ajax({
+            url: BASE_URL,
+            method: 'POST',
+            contentType: 'application/json',
+            data: newBookmark,
+            success: callback,
         });
     });
 
-    console.log('getAPI ran');
-
     return {
         getBookmarks,
+        createBookmarks,
     };
+
+    
 
 }());
--- scripts/index.js
+++ scripts/index.js
@@ -5,9 +5,16 @@ $(function() {
     mainRender();
     console.log(`this is what's in the store: ${store.items}`);
 
-    getAPI.getBookmarks((items) => {
+    api.getBookmarks((items) => {
+        console.log()
         items.forEach((item) => {
         console.log(item);
         });
     });
+
+    api.createBookmarks('brnr', (bookmark) => {
+        api.getBookmarks((bookmarks) => {
+          console.log(bookmarks);
+        });
+      });
 });
