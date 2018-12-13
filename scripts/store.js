ac6f555013a1920fe9863e651791789550748381
publikwerker <hello@publikwerks.com>
2018-12-13 09:40:28 -0800

    linked css and jquery and added store object

--- scripts/store.js
+++ scripts/store.js
@@ -1,1 +1,16 @@
 console.log('store.js ran');
+
+const store = {
+    items: [ 
+        {
+            id: 'id',
+            name: 'name',
+            blurb: 'blurb',
+            link: 'link',
+            isExpanded: false,
+        },
+    ],
+    isAdding: false,
+
+};
+console.log(`this is what's in the store: ${store.items}`);
